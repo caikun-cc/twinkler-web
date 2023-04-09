926b3cb8f26d9a642ab86e92faace454fe218a36
Caikun <tskunc@qq.com>
2023-04-09 22:48:26 +0800

    4.9-0

--- src/layout/ConsoleLayout.vue
+++ src/layout/ConsoleLayout.vue
@@ -3,36 +3,37 @@
         <el-aside width="240px">
             <div class="logo-side-container"
                  @click="navigationTo('index')"
-                 style="cursor: pointer"
-            >
+                 style="cursor: pointer">
                 <span>Twinkler</span>
             </div>
-            <el-menu style="border: none;">
-                <el-menu-item>
+            <el-menu style="border: none;"
+                     active-text-color="#fa983a"
+                     default-active="console">
+                <el-menu-item index="console">
                     <el-icon>
                         <Compass/>
                     </el-icon>
                     <span>首页</span>
                 </el-menu-item>
-                <el-menu-item>
+                <el-menu-item index="gallery">
                     <el-icon>
                         <PictureRounded/>
                     </el-icon>
                     <span>图库</span>
                 </el-menu-item>
-                <el-menu-item>
+                <el-menu-item index="albums">
                     <el-icon>
                         <Promotion/>
                     </el-icon>
                     <span>相册</span>
                 </el-menu-item>
-                <el-menu-item>
+                <el-menu-item index="user">
                     <el-icon>
                         <User/>
                     </el-icon>
                     <span>个人</span>
                 </el-menu-item>
-                <el-menu-item>
+                <el-menu-item index="settings">
                     <el-icon>
                         <Setting/>
                     </el-icon>
@@ -69,6 +70,10 @@ export default {
 <style scoped>
 .console-root {
     height: 100vh;
+}
+
+.el-menu-item:hover {
+    background: #f7f1e3;
 }
 
 .logo-side-container {
