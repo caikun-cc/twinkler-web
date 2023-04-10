1d1ffd79ae956e596763d1463e3ab97afc760c62
pyzzd <7698346+pyzzd@user.noreply.gitee.com>
2023-04-10 22:27:54 +0800

    添加了注册页面，完成了邮件发送

--- src/components/account/Register.vue
+++ src/components/account/Register.vue
@@ -3,23 +3,23 @@
     <p class="login-text">注册</p>
     <el-form size="large">
       <el-form-item>
-        <el-input placeholder="输入昵称"/>
+        <el-input v-model="User.nick" placeholder="输入昵称"/>
       </el-form-item>
       <el-form-item>
-        <el-input placeholder="输入邮箱"/>
+        <el-input v-model="User.email" placeholder="输入邮箱"/>
       </el-form-item>
       <el-form-item>
         <el-row style="width: 100%;justify-content: space-between">
           <el-col :span="16">
-            <el-input placeholder="输入验证码"/>
+            <el-input v-model="User.verifyCode" placeholder="输入验证码"/>
           </el-col>
           <el-col :span="7">
-            <el-button style="display: block;width: 100%">发送</el-button>
+            <el-button style="display: block;width: 100%" @click="send()">发送</el-button>
           </el-col>
         </el-row>
       </el-form-item>
       <el-form-item>
-        <el-input placeholder="输入密码"/>
+        <el-input v-model="User.password" placeholder="输入密码"/>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" style="width: 100%">注册</el-button>
@@ -35,12 +35,56 @@
 <script>
 
 import router from "../../router/router.js";
+import {ElNotification} from 'element-plus'
+import {sendEmail} from "../../http/apis";
 
 export default {
   name: "Register",
+  data() {
+    return {
+      User: {
+        nick: '',
+        email: '',
+        verifyCode: '',
+        password: '',
+      }
+    }
+  },
   methods: {
     router() {
       return router
+    },
+    send() {
+      if (this.verifyNick() && this.verifyEmail()){
+        sendEmail(this.User.email).then(r=>{
+            if (r.status){
+              ElNotification({message: '邮件发送成功', title: 'Success', type: 'success'})
+            }else{
+              ElNotification({message: '邮件发送异常', title: 'Error', type: 'error'})
+            }
+        }).catch(e=>{
+          ElNotification({message: e.message, title: 'Error', type: 'error'})
+        })
+
+      }
+    },
+    verifyNick(){
+      var special = new RegExp(/[^a-zA-Z0-9]/);
+      if (this.User.nick.length < 6 || this.User.nick.length >= 18) {
+        ElNotification({message: '昵称长度为6-18位', title: 'Error', type: 'error',})
+      } else if (special.test(this.User.nick)) {
+        ElNotification({message: '昵称不得包含特殊字符', title: 'Error', type: 'error',})
+      }else{
+        return true
+      }
+    },
+    verifyEmail(){
+      var email_Regex = new RegExp('^.+@[A-Z0-9a-z]+\.[a-zA-Z]+$');
+      if (!email_Regex.test(this.User.email)) {
+        ElNotification({message: '邮箱不合法', title: 'Error', type: 'error',})
+      }else{
+        return true
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import router from "./router/router.js";
 
 import './assets/style.css'
+import "element-plus/theme-chalk/el-loading.css";
+import "element-plus/theme-chalk/el-message.css";
+import "element-plus/theme-chalk/el-notification.css";
+import "element-plus/theme-chalk/el-message-box.css";
 
 const app = createApp(App)
 app.use(router)
